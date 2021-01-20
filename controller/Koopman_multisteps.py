01614ff88bc3e6021ac9b2b61a059f1bb3594a8d
juhanw <juhanw@umich.edu>
2021-01-20 15:12:03 -0500

    multisteps

---
+++ controller/Koopman_multisteps.py
@@ -0,0 +1,172 @@
+import numpy as np
+
+class Koopman:
+    def __init__(self, Xub, Xlb, Uub, Ulb, num_basis=2, weighting=0.96):
+        """
+        potential improvements:
+        1. delay
+        2. sparse regression to denoise
+        """
+        self.n = np.size(Xub)
+        self.m = np.size(Uub)
+        self.nk = num_basis
+        self.weighting = weighting
+        self.Xub = Xub.reshape(1,self.n)
+        self.Xlb = Xlb.reshape(1,self.n)
+        self.Uub = Uub.reshape(1,self.m)
+        self.Ulb = Ulb.reshape(1,self.m)
+
+        # set scale center and range
+        Xub_scale = self.Xub + 0.3*(self.Xub-self.Xlb)
+        Xlb_scale = self.Xlb - 0.3*(self.Xub-self.Xlb)
+        Uub_scale = self.Uub + 0.3*(self.Uub-self.Ulb)
+        Ulb_scale = self.Ulb - 0.3*(self.Uub-self.Ulb)
+        self.state_range = (Xub_scale - Xlb_scale) / 2
+        self.state_center = (Xub_scale + Xlb_scale) / 2
+        self.action_range = (Uub_scale - Ulb_scale) / 2
+        self.action_center = (Uub_scale + Ulb_scale) / 2
+        print('Set up scale ranges and centers successfully!')
+
+    def initialization(self, states, actions):
+        """
+        scale data to [-1,1]
+        data = Nt x N
+        construct Koopman matrices A, B, C
+        initialize recursive update matrices G, beta
+        z = A*x + B*u
+        x = C*z
+        Koopman library functions: RFF
+        """
+        # set rff:
+        rff_sigma_gaussian = np.std(states)
+        num_features = int((self.nk - self.n)/2)
+        self.rff_z = np.random.randn(self.n, num_features)/rff_sigma_gaussian
+        print('Set up Random Fourier Features successfully!')
+
+        states_scaled = self.scale(states)
+        actions_scaled = self.scale(actions,state_scale=False)
+        X_scaled = states_scaled[:-1,:]
+        Y_scaled = states_scaled[1:,:]
+        U_scaled = actions_scaled[1:,:] 
+        Nt = np.size(X_scaled,0)
+        Weights = np.sqrt(self.weighting)**range(Nt-1,-1,-1)
+        # Weights = Weights.reshape(Nt,1)
+        self.X = Weights*X_scaled.T
+        self.Y = Weights*Y_scaled.T
+        self.U = Weights*U_scaled.T
+        self.PsiX = self.lift(self.X.T)
+        self.PsiY = self.lift(self.Y.T)
+        # self.X = self.X.reshape(self.n,Nt)
+        # self.Y = self.Y.reshape(self.n,Nt)
+        # self.U = self.U.reshape(self.m,Nt)
+        self.Zeta = np.vstack((self.PsiX,self.U))
+        self.non_singular = 0.1
+        self.Q = np.matmul(self.PsiY,self.Zeta.T)
+        self.G = np.linalg.inv(np.matmul(self.Zeta,self.Zeta.T) + self.non_singular*np.eye(len(self.Zeta)))
+        # self.G = np.linalg.inv(np.matmul(self.Zeta,self.Zeta.T))
+        self.AB = np.matmul(self.Q,self.G)
+        self.A = self.AB[:,:self.nk]
+        self.B = self.AB[:,self.nk:]
+        self.C = np.hstack((np.eye(self.n),np.zeros((self.n,self.nk-self.n))))
+        self.G = self.G /self.weighting
+        self.G = (self.G + self.G.T)/2
+        # Evaluate regression accuracy:
+        error = self.Y - np.matmul(self.C,np.matmul(self.AB,self.Zeta))
+        NRMSE_Koopman = 100*np.sqrt(sum(np.linalg.norm(error,axis=0)**2)) / np.sqrt(sum(np.linalg.norm(self.Y,axis=0)**2))
+        print(NRMSE_Koopman,"%")
+
+        return self.A, self.B, self.C
+
+    def scale(self, data, scale_down=True, state_scale=True):
+        """
+        data is a Nt x N matrix
+        scale down to [-1,1], scale up to stored scaling range
+        initialize scaling range with first initialization data set
+        """ 
+        if np.shape(data) == (self.m,1) or np.shape(data) == (self.n,1):
+            data = data.T
+        if state_scale:
+            if scale_down:
+                scaled = (data - self.state_center) / self.state_range
+            else:
+                scaled = data*self.state_range + self.state_center
+        else:
+            if scale_down:
+                scaled = (data - self.action_center) / self.action_range
+            else:
+                scaled = data*self.action_range + self.action_center
+        
+        return scaled
+
+    def lift(self, data):
+        """
+        data is a Nt x N matrix
+        return lifted states Nk x Nt matrix (s.t A*Psi)
+        lift the state space to a Koopman subspace
+        lifted = [states; (actions?); lift(states)]
+        RFF sampling rff_z ~ N(0, sigma^2*I_n)
+        # RBF
+        """
+        Q = np.matmul(data,self.rff_z)
+        Fcos = np.cos(Q)
+        Fsin = np.sin(Q)
+        F = np.hstack((Fcos, Fsin))/ np.sqrt(np.size(self.rff_z,1))
+        Psi = np.hstack((data,F))
+        Psi = Psi.T
+
+        return Psi
+
+    def update(self, states_x, states_y, actions):
+        """
+        recursive update AB, G 
+        states is 1 x n
+        actions is 1 x m
+        scale_down -> update
+        """
+        statesx_scaled = self.scale(states_x.reshape(1,self.n))
+        statesy_scaled = self.scale(states_y.reshape(1,self.n))
+        actions_scaled = self.scale(actions.reshape(1,self.m),state_scale=False)
+        x_new = statesx_scaled.reshape(self.n,1)
+        y_new = statesy_scaled.reshape(self.n,1)
+        u_new = actions_scaled.reshape(self.m,1)
+        delta = np.vstack((self.lift(x_new.reshape(1,self.n)),u_new))
+        calc_easy = np.matmul(self.G,delta)
+        beta = 1/(1 + np.matmul(delta.T,calc_easy))
+        innovation = self.lift(y_new.reshape(1,self.n)) - np.matmul(self.AB,delta)
+        self.AB += beta*np.matmul(innovation,calc_easy.T)
+        self.A = self.AB[:,:self.nk]
+        if np.max(self.A) > 1000:
+            print("SOmething wrong") # G has become negative definite det = -5e136 <-- calc_easy exp grow
+        self.B = self.AB[:,self.nk:]
+        self.G = (self.G - beta*np.matmul(calc_easy,calc_easy.T))/self.weighting
+        self.G = (self.G + self.G.T)/2
+        print(np.linalg.det(self.G))
+        if np.linalg.det(self.G) < 0:
+            temp = beta*np.matmul(calc_easy,calc_easy.T)
+            rank = np.linalg.eigvals(temp)
+            print(min(rank))
+            GG = np.linalg.inv(np.matmul(self.Zeta,self.Zeta.T) + self.non_singular*np.eye(len(self.Zeta)))
+            GG = GG /self.weighting
+            GG = (GG + GG.T)/2
+            self.G = GG
+        
+        self.X = np.hstack((self.X,x_new))
+        self.Y = np.hstack((self.Y,y_new))
+        self.U= np.hstack((self.U,u_new))
+        self.Zeta = np.hstack((self.Zeta,delta))
+        return self.A, self.B, self.C
+
+    def predict(self, states, actions):
+        """
+        scale_down -> Koopman_predict -> scale_up
+        """
+        states = states.reshape(1,self.n)
+        actions = actions.reshape(1,self.m)
+        states_scaled = self.scale(states)
+        actions_scaled = self.scale(actions,state_scale=False)
+        delta_new = np.vstack([self.lift(states_scaled),actions_scaled.reshape(self.m,1)])
+        lift_predicted = np.matmul(self.AB,delta_new)
+        predicts_scaled = np.matmul(self.C, lift_predicted)
+        predicted = self.scale(predicts_scaled.reshape(1,self.n),scale_down=False)
+
+        return predicted
---
+++ controller/test multisteps.py
@@ -0,0 +1,254 @@
+# Import python libraries 
+import pystorms
+# import sys
+# sys.path.append("/controller")
+# from denseMPC import MPC
+# from MPC_rewrite import MPC
+# from MPC_soft import MPC
+from MPC_smooth import MPC
+# from Koopman import Koopman 
+from Koopman_rewrite import Koopman
+from Koopman_multisteps import Koopman
+# Python Scientific Computing Stack
+import numpy as np
+import pandas as pd
+# Plotting tools
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+# KMPC
+env_equalfilling = pystorms.scenarios.delta() # states = [BC, BS, N1, N2, N3, N4]
+done = False
+
+u = []
+x0 = []
+actions_north3 = []
+actions_north2 = []
+actions_north1 = []
+actions_central = []
+actions_south = []
+settings = np.ones(5)
+Xub_extreme = np.asarray([5.7, 9.5, 5.92, 6.59, 11.99]) # [BC, BS, N1, N2, N3]
+Xlb_extreme = np.asarray([2.21, 0, 2.11, 4.04, 5.28])
+Xub_allowed = np.asarray([3.8, 6.55, 5.8, 5.04, 5.92])
+Xlb_allowed = np.asarray([3.28, 0, 5.2, 4.44, 5.32])
+Uub = np.ones((1,5))
+Ulb = np.zeros((1,5))
+
+t = 0
+tmulti = 30
+n_basis = 6
+n = Xub_extreme.size
+m = Uub.size
+nk = n + n_basis
+n0 = 20    #n0 > nk + m
+KPmodel = Koopman(Xub_extreme,Xlb_extreme,Uub,Ulb,nk)
+Xub_scaled = KPmodel.scale(Xub_extreme)
+Xlb_scaled = KPmodel.scale(Xlb_extreme)
+Xub_allowed_scaled = KPmodel.scale(Xub_allowed)
+Xlb_allowed_scaled = KPmodel.scale(Xlb_allowed)
+Uub_scaled = KPmodel.scale(Uub,state_scale=False)
+Ulb_scaled = KPmodel.scale(Ulb,state_scale=False)
+KMPC = MPC(Uub_scaled,Ulb_scaled, Xub_soft= Xub_scaled, Xlb_soft = Xlb_scaled)
+
+while not done:
+    if  t <= n0:
+        actions =  np.random.uniform(0,0.5,5)
+        u.append(actions)
+        actions_north3.append(actions[4])
+        actions_north2.append(actions[3])
+        actions_north1.append(actions[2])
+        actions_central.append(actions[0])
+        actions_south.append(actions[1])
+
+        done = env_equalfilling.step(actions)
+        state = env_equalfilling.state()
+        x0.append(state[:-1])
+
+        if t == n0:
+            u = np.asarray(u)
+            x0 = np.asarray(x0)
+            u.reshape(n0+1,m)
+            x0.reshape(n0+1,n)
+            
+            # initialize Koopman model:
+            A,B,C = KPmodel.initialization(x0,u)
+            t1 = t
+
+            # get control input
+            z0 = KPmodel.lift(KPmodel.scale(x0[-1,:])) # x
+            ulast_scaled = KPmodel.scale(u[-1,:],state_scale=False)
+            actions_mpc = KMPC.getMPC(z0,ulast_scaled,A,B,C)
+            if actions_mpc is None:
+                actions_mpc = u[-1,:]
+            actions = KPmodel.scale(actions_mpc,scale_down=False,state_scale=False)
+            actions = actions.T
+            actions_north3.append(actions[4])
+            actions_north2.append(actions[3])
+            actions_north1.append(actions[2])
+            actions_central.append(actions[0])
+            actions_south.append(actions[1])
+            
+            # initialize containers
+            xtrue = x0[-1,:].reshape(1,5)
+            xkp = xtrue
+            umpc = actions.reshape(1,5)
+            xkp = np.vstack((xkp,KPmodel.predict(xtrue,umpc) ))
+
+    else:
+        done = env_equalfilling.step(umpc[-1,:])
+        state = env_equalfilling.state() # y
+        # states = [state[2], state[1], state[0], state[4], state[5]]
+        xtrue = np.vstack((xtrue,state[:-1].reshape(1,5) ))
+
+        if t-t1 == tmulti:
+            # update Koopman model
+            A,B,C = KPmodel.update(xtrue[-1,:],xtrue[-2,:],umpc[-1,:])
+            t1 = t
+            xkp_new = KPmodel.predict(xtrue[-1,:],umpc[-1,:]).reshape(1,np.size(xkp,1))
+            xkp = np.vstack((xkp, xkp_new))
+            # get control input
+            z0 = KPmodel.lift(KPmodel.scale(xtrue[-1,:]))
+            ulast_scaled = KPmodel.scale(umpc[-1,:],state_scale=False)
+        else:
+            xkp_new = KPmodel.predict(xkp[-1,:],umpc[-1,:]).reshape(1,np.size(xkp,1))
+            xkp = np.vstack((xkp, xkp_new))
+            # get control input
+            z0 = KPmodel.lift(KPmodel.scale(xkp[-1,:]))
+            ulast_scaled = KPmodel.scale(umpc[-1,:],state_scale=False)
+        
+        actions_mpc = KMPC.getMPC(z0,ulast_scaled,A,B,C)
+        if actions_mpc is None:
+            actions_mpc = umpc[-1,:]
+        actions = KPmodel.scale(actions_mpc,scale_down=False,state_scale=False)
+        actions = actions.T
+        actions_north3.append(actions[4])
+        actions_north2.append(actions[3])
+        actions_north1.append(actions[2])
+        actions_central.append(actions[0])
+        actions_south.append(actions[1])
+        umpc = np.vstack((umpc,actions.reshape(1,np.size(umpc,1)) ))
+        xkp_all = xkp[:-1,:]
+        qoi = t-50
+        error = xtrue - xkp_all
+        NRMSE_predict = 100*np.sqrt(sum(np.linalg.norm(error[qoi:,:],axis=0)**2)) / np.sqrt(sum(np.linalg.norm(xtrue[qoi:,:],axis=0)**2))
+        if NRMSE_predict < 0.5 and qoi > 0:
+            print(NRMSE_predict)
+
+    print(t, "is time")
+    t = t + 1
+    if t > 3500:
+        # break
+        print(t, "is time")
+    
+equalfilling_perf = sum(env_equalfilling.data_log["performance_measure"])
+
+
+
+################################################################################
+# Plotting
+################################################################################
+# set seaborn figure preferences and colors
+sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
+sns.set_style("darkgrid")
+colorpalette = sns.color_palette("colorblind")
+colors_hex = colorpalette.as_hex()
+plt.rcParams['figure.figsize'] = [20, 15]
+plt.rcParams['figure.dpi'] = 100 # 200 e.g. is really fine, but slower
+
+# plot on Koopman performance ===================================================
+# Evaluate regression accuracy:
+xkp_all = xkp[:-1,:]
+qoi = 0
+error = xtrue - xkp_all
+NRMSE_predict = 100*np.sqrt(sum(np.linalg.norm(error[qoi:,:],axis=0)**2)) / np.sqrt(sum(np.linalg.norm(xtrue[qoi:,:],axis=0)**2))
+print(NRMSE_predict,"%")
+label1 = "Koopman model NRMSE = "+str(round(NRMSE_predict,3))+"%"
+for i in range(5):
+    plt.subplot(2,3,i+1)
+    plt.plot(xtrue[qoi:,i], '-', label = "Ground Truth")
+    plt.plot(xkp_all[qoi:,i], '--', label = label1)
+    plt.title("state"+str(i+1))
+plt.legend(loc="upper right", borderaxespad=0.1)
+plt.tight_layout()
+plt.show()
+# print("done")
+
+# '''
+# plot on states =============================================================
+plotenvironment = env_equalfilling
+
+plt.subplot(2, 3, 1)
+plt.plot(plotenvironment.data_log["flow"]["conduit_Eout"], color=colors_hex[0])
+plt.axhline(12.0, color="r")
+plt.ylim([0,20])
+plt.title("Conduit East Out")
+plt.ylabel("Flow")
+
+plt.subplot(2, 3, 2)
+plt.plot(plotenvironment.data_log["depthN"]["basin_C"], color=colors_hex[0])
+plt.axhline(5.7, color="r")
+plt.axhline(3.8, color="k")
+plt.axhline(3.28, color="k")
+plt.axhline(2.21, color="r")
+plt.ylim([0,11.5])
+plt.ylabel("Depth")
+plt.title("Central Basin")
+
+
+plt.subplot(2, 3, 3)
+plt.plot(plotenvironment.data_log["depthN"]["basin_S"], color=colors_hex[0])
+plt.axhline(9.5, color="r")
+plt.axhline(6.55, color="k")
+plt.ylim([0,12])
+plt.ylabel("Depth")
+plt.title("South Basin")
+
+plt.subplot(2, 3, 4)
+plt.plot(plotenvironment.data_log["depthN"]["basin_N1"], color=colors_hex[0])
+plt.axhline(5.92, color="r")
+plt.axhline(2.11, color="r")
+plt.axhline(5.8, color="k")
+plt.axhline(5.2, color="k")
+plt.ylim([0,12])
+plt.ylabel("Depth")
+plt.title("Basin North1")
+
+plt.subplot(2, 3, 5)
+plt.plot(plotenvironment.data_log["depthN"]["basin_N2"], color=colors_hex[0])
+plt.axhline(6.59, color="r")
+plt.axhline(5.04, color="k")
+plt.axhline(4.44, color="k")
+plt.axhline(4.04, color="r")
+plt.ylim([0,12])
+plt.ylabel("Depth")
+plt.title("Basin North2")
+
+plt.subplot(2, 3, 6)
+plt.plot(plotenvironment.data_log["depthN"]["basin_N3"], color=colors_hex[0])
+plt.axhline(11.99, color="r")
+plt.axhline(5.92, color="k")
+plt.axhline(5.32, color="k")
+plt.axhline(5.28, color="r")
+plt.ylim([0,16])
+plt.ylabel("Depth")
+plt.title("Basin North3")
+plt.show()
+
+
+# new plot on control =============================================================
+plt.rcParams['figure.figsize'] = [6, 6]
+
+plt.plot(actions_north3, label='North3 Weir', linestyle='-', linewidth=3.0)
+plt.plot(actions_north2, label='North2 Weir', linestyle='--', linewidth=2.0)
+plt.plot(actions_north1, label='North1 Weir', linestyle='--', linewidth=2.0)
+plt.plot(actions_central, label='Central Weir', linestyle='-', linewidth=2.0)
+plt.plot(actions_south, label='South Orifice', linestyle='-.', linewidth=2.0)
+
+plt.ylim([-0.1,1.1])
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.suptitle('Control settings')
+plt.ylabel('Control asset setting')
+plt.xlabel('Simulation step')
+plt.show()
+# '''
